ecfd1be269f13954b9ece742e4aa73eec2424a63
kana333 <you@example.com>
2019-11-09 12:04:32 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update, :destory]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
